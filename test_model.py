bda1f76c340d6a239adc06048487e7b14c17cf15
Theertha <theertha1808@gmail.com>
2024-03-15 15:26:16 +0100

    Updated code

--- test_model.py
+++ test_model.py
@@ -1,5 +1,8 @@
 from CNN_Model import *
 import tensorflow as tf
+import time
+import numpy as np
+import pandas as pd
 
 # Assuming the following functions are defined as in your previous code:
 # read_and_prepare_data(dataset_path)
@@ -30,18 +33,19 @@ def predict_peaks(csv_path, model_path, window_width, Fs):
     predictions = model.predict(filtered_signals)
 
     # Convert predictions to peak positions or other relevant metric
-    peak_predictions = np.argmax(predictions, axis=1) * window_width
+    peak_predictions = np.argmax(predictions, axis=1) * window_width + 50
 
     return peak_predictions
 
 if __name__ == "__main__":
     start_time = time.time()
     saved_model_path = r'D:\AIS_ML\AIS_ML\Output\cnn_model_test.h5'
-    dataset_path = r'D:\AIS_ML\AIS_ML\Dataset\adc_1m_hard_surface.csv'
+    dataset_path = r'D:\AIS_ML\AIS_ML\Dataset\adc_1m_hard_surface_2.csv'
     window_width = 64
     Fs = 1953125  # Sampling frequency in Hz
     dt = 1 / Fs
     df = read_and_prepare_data(dataset_path)
+    print(df)
     peaks, distances, filtered_signals = reduce_noise_and_label(df, dt)
     signal_length = len(df.columns)
     y_label = group_labeled_data(peaks, signal_length, window_width)
@@ -53,4 +57,6 @@ if __name__ == "__main__":
     peak_differences = np.abs(peak_predictions - peaks)
     print("Differences between actual and predicted peak positions:", peak_differences)
 
+    end_time = time.time()
+    print("Time taken to predict:", end_time - start_time, "seconds")
 
